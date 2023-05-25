58949835e298e10ac183c138cef888599885b15c
Lars Trieloff <lars@trieloff.net>
2023-05-25 11:59:46 +0200

    fix: correct user name and email

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ echo $ORG is the org name
 echo $REPO is the repo name
 echo run as $USER_NAME $USER_EMAIL
 
-git config --global user.name "$USER"
-git config --global user.email "$USER"
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
 
 mkdir parent
 # checkout the parent repo
