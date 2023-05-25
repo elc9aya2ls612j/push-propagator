64af0ec040e73fa4dd7fe2ccd1e50a9046f8ad22
Lars Trieloff <lars@trieloff.net>
2023-05-25 11:25:51 +0200

    feat: call setup from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,23 @@ ALWAYS_OVERWRITE=$3
 USER_NAME=$4
 USER_EMAIL=$5
 OWNER=$6
-REPO=$7
+# take owner/rep and keep only the repo
+REPO=$(echo $7 | cut -d'/' -f 2)
 REF=$8
 
-echo $OWNER is the org name
-echo $REPO is the repo name
-echo $REF is the ref name
-echo run as $USER_NAME $USER_EMAIL
+source scripts/build.sh
 
+cd parent
+FORKS=$(gh repo list $ORG_NAME --json parent,name -q '.[] | select(.parent != null and .parent.name == "'$REPO_NAME'") | .name')
+echo "We have these forks: $FORKS"
+
+# Loop through the forks
+for FORK in $FORKS; do
+    echo $FORK is the current fork
+    # if the fork is a custom
+    if gh api /repos/$ORG_NAME/$FORK/topics --jq ".names" | grep -q custom; then
+        source $SCRIPTS_PATH/custom.sh
+    else
+        source $SCRIPTS_PATH/standard.sh
+    fi
+done
