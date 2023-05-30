c16771e3471b5de5b7e5c7e87584d4610ba5f161
freshjones <william.jones@pfizer.com>
2023-05-30 16:47:46 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ USER_EMAIL=$5
 ORG=$(echo $GITHUB_REPOSITORY | cut -d / -f 1)
 REPO=$(echo $GITHUB_REPOSITORY | cut -d / -f 2)
 
-
 git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
 
@@ -26,6 +25,10 @@ echo $ORG is the org name
 echo $REPO is the repo name
 echo run as $USER_NAME $USER_EMAIL
 echo $REF is the ref name
+
+#auth into GitHub CLI
+echo $GITHUB_TOKEN | gh auth login --with-token
+gh auth status
 
 FORKS=$(gh repo list $ORG --fork --json parent,name -q '.[] | select(.parent != null and .parent.name == "'$REPO'") | .name')
 echo "We have these forks: $FORKS"
