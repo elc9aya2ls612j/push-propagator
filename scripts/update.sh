189fca9d1f51616d92f00c62f12a1b3a4534de88
William Jones <billy@freshjones.com>
2023-06-04 22:06:20 -0400

    making auto merge possible

--- scripts/update.sh
+++ scripts/update.sh
@@ -84,7 +84,7 @@ fi
 # if gh api /repos/$ORG_NAME/$FORK/topics --jq ".names" | grep template; then
 if $AUTO_MERGE; then
     echo "we are gonna auto merge"
-    gh pr merge --admin --auto --delete-branch --merge "merge-$REF_NAME"
+    gh pr merge --admin --delete-branch --merge "merge-$REF_NAME"
 fi
 
 # Go back to the parent repo
